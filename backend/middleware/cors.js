faf289db8b661b61c5bf81259d2e23f52f9586a6
sarangraii <sarang2452@gmail.com>
2025-07-26 00:45:53 +0530

    cors error

--- backend/middleware/cors.js
+++ backend/middleware/cors.js
@@ -4,7 +4,8 @@ const corsOptions = {
   origin: [
     'http://localhost:3000',
     'http://localhost:3001',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://my-pushtak-ltao.vercel.app/'
   ],
   credentials: true,
   optionsSuccessStatus: 200,
